53ef8b204c5554537b37779098f13fb307cdf324
Andrei Romanciuc <56957259+andreiromanciuc@users.noreply.github.com>
2021-01-19 19:03:29 +0200

    configured security and added logout button

--- src/main/java/com/andrei/fleetManagement/webController/CustomerController.java
+++ src/main/java/com/andrei/fleetManagement/webController/CustomerController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("*/customer")
 public class CustomerController {
 
     private final CustomerService customerService;
